d025c34c7ee1b5f52e0d97dc9132aaf59e7ba85f
Roman-Sergeichuk <sergeichuk.roman@mail.ru>
2020-08-31 02:34:47 +0300

    Formatting of the SQL-query

--- task_2/SQL_query.sql
+++ task_2/SQL_query.sql
@@ -3,7 +3,5 @@ SELECT T1.date, T1.result, COUNT(T1.result) AS results_count, SUM(T1.length) AS 
 	JOIN project AS T2 ON T1.project_id = T2.id 
 	JOIN server AS T3 ON T1.server_id = T3.id 
 	GROUP BY T1.result, T1.date, T2.name, T3.name 
-	HAVING T1.date 
-	BETWEEN '2020-08-27' 
-	AND '2020-08-31' 
+	HAVING T1.date BETWEEN '2020-08-27' AND '2020-08-31' 
 	ORDER BY T1.date DESC, T1.result;
